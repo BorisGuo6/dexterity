89a021fd78c62dbf6e84712195defe45e81b7a3d
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-10-21 20:50:07 -0400

    updates 10/21, reliability testing

--- code/ESPNOW/RoboticArm_ESPNOW/RoboticArm_ESPNOW.ino
+++ code/ESPNOW/RoboticArm_ESPNOW/RoboticArm_ESPNOW.ino
@@ -1,21 +1,37 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <stdint.h>
 
 #define ESPNOW_WIFI_CHANNEL 5            // WiFi channel to be used by ESP-NOW. The available channels will depend on your region.
 #define ESPNOW_WIFI_MODE    WIFI_STA     // WiFi mode to be used by ESP-NOW. Any mode can be used.
 #define ESPNOW_WIFI_IF      WIFI_IF_STA  // WiFi interface to be used by ESP-NOW. Any interface can be used.
+#define DATA_RATE           100          // In Hz
 
 // Define the data structure to send
 typedef struct position_packet {
   char str[32];
-  int finger_position_1;
-  int finger_angle_1;
-  int wrist_position;
-  int wrist_angle;
+  int messages_rec;
+  uint8_t pos1;
+  uint8_t pos2;
+  uint8_t pos3;
+  uint8_t pos4;
+  uint8_t pos5;
+  uint8_t pos6;
+  uint8_t pos7;
+  uint8_t pos8;
+  uint8_t pos9;
+  uint8_t pos10;
+  uint8_t pos11;
+  uint8_t pos12;
+  uint8_t pos13;
+  uint8_t pos14;
+  uint8_t pos15;
+  uint8_t pos16;
 } position_packet;
 
 typedef struct haptic_packet {
   char str[32];
+  int messages_rec;
   int finger_sensor_1;
   int finger_sensor_2;
   int finger_sensor_3;
@@ -24,6 +40,13 @@ typedef struct haptic_packet {
 // Create an instance of the struct to be sent
 position_packet inData;
 haptic_packet outData;
+uint8_t pos5_in;
+int sizeIn;
+
+int messages_send_attempt;
+int messages_send_success;
+int messages_recv;
+int i;
 
 
 // Peer MAC Address
@@ -43,25 +66,35 @@ void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int 
   } else {
       Serial.println("Received data length does not match expected size.");
   }
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Message: ");
-  Serial.println(inData.str);
-  Serial.print("finger_position_1: ");
-  Serial.println(inData.finger_position_1);
-  Serial.print("finger_angle_1: ");
-  Serial.println(inData.finger_angle_1);
-  Serial.print("wrist_position: ");
-  Serial.println(inData.wrist_position);
-  Serial.print("wrist_angle: ");
-  Serial.println(inData.wrist_angle);
-  Serial.println();
+  // Serial.print("Bytes received: ");
+  // Serial.println(len);
+  // Serial.print("Message: ");
+  // Serial.println(inData.str);
+  // Serial.print("finger_position_1: ");
+  // Serial.println(inData.finger_position_1);
+  // Serial.print("finger_angle_1: ");
+  // Serial.println(inData.finger_angle_1);
+  // Serial.print("wrist_position: ");
+  // Serial.println(inData.wrist_position);
+  // Serial.print("wrist_angle: ");
+  // Serial.println(inData.wrist_angle);
+  // Serial.println();
+  messages_send_success = inData.messages_rec;
+  messages_recv++;
+  pos5_in = inData.pos5;
+  sizeIn = len;
 }
 
 
 void setup() {
   // Start the Serial Monitor
   Serial.begin(115200);
+
+  messages_send_attempt = 0;
+  messages_send_success = 0;
+  messages_recv = 0;
+  i = 0;
+
   while (!Serial) {
     delay(10);
   }
@@ -79,9 +112,6 @@ void setup() {
   // Print the MAC address of this device
   Serial.print("MAC Address: ");
   Serial.println(WiFi.macAddress());
-
-  // DELAY: enter delay if trying to sync up MAC addresses
-  // delay(10000);
 
   // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -106,17 +136,22 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+
+  // DELAY: enter delay if trying to sync up MAC addresses
+  delay(10000);
 }
 
 void loop() {
   // Set values to send
   strcpy(outData.str, "Haptic Data");
-  outData.finger_sensor_1 = random(1, 20);
   outData.finger_sensor_2 = random(1, 20);
   outData.finger_sensor_3 = random(1, 20);
+  outData.messages_rec = messages_recv;
 
   // Send struct message via ESP-NOW
   esp_err_t result = esp_now_send(peer_mac, (uint8_t *)&outData, sizeof(outData));
+
+  messages_send_attempt += 1;
 
   if (result == ESP_OK) {
     // Serial.println("Sent with success");
@@ -124,6 +159,23 @@ void loop() {
     Serial.println("Error sending the data");
   }
 
-  delay(4000);  // Wait 4 seconds before sending again
+  delay(1000/DATA_RATE);  // Wait before sending again
+
+  i += 1;
+  if(i % 200 == 0){
+    Serial.println();
+    Serial.print("Messages Sent: ");
+    Serial.println(messages_send_attempt);
+    Serial.print("Messages Delivered: ");
+    Serial.println(messages_send_success);
+    Serial.print("Success rate: ");
+    Serial.print(messages_send_success*100/messages_send_attempt);
+    Serial.println(" %");
+    Serial.print("position 5: ");
+    Serial.println(pos5_in);
+    Serial.print("size in: ");
+    Serial.println(sizeIn);
+    Serial.println();
+  }
 }
 
