3f03caac1e9d45bff986d4981b42cef9282bab6a
Max Titov <max.teetov@gmail.com>
2024-11-26 18:32:30 -0500

    finger tracking lib is creating less noisy data now

--- code/glove/test/HallEffectLibTest/lib/FingerTracking/FingerTracking.cpp
+++ code/glove/test/HallEffectLibTest/lib/FingerTracking/FingerTracking.cpp
@@ -102,6 +102,7 @@ void adjustAngles()
 void calcFingerAngles()
 {
     measureHallEffectSensors();
+    calibrateHallEffectSensors();
     adjustAngles();
 }
 
--- code/glove/test/HallEffectLibTest/src/main.cpp
+++ code/glove/test/HallEffectLibTest/src/main.cpp
@@ -33,7 +33,6 @@ void setup() {
 
 void loop() {
 	calcFingerAngles();
-	calibrateHallEffectSensors();
 	printFingerAngles();
 
 	sendData();
