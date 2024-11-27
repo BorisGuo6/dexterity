a2a7244d8c43424fb250d14bef5083355c75599e
Max Titov <max.teetov@gmail.com>
2024-11-26 22:49:53 -0500

    fixed thumb movement on control glove side

--- code/RoboticArm/test/ArmControlLibTest/lib/FingerControl/FingerControl.cpp
+++ code/RoboticArm/test/ArmControlLibTest/lib/FingerControl/FingerControl.cpp
@@ -10,6 +10,7 @@ int* metacarpalServoCalc(int MCP_flexion, int MCP_abduction, bool flip_abduction
 	int servo1_offset = 0;
 
     //calculate how much the servos should offset from each other for abduction
+    
 	if(MCP_abduction < 0){ //right
 		MCP_abduction = max(MCP_abduction,MCP_ABDUCTION_MIN);
 		servo1_offset=-MCP_abduction;
@@ -52,13 +53,16 @@ int* thumbCMCServoCalc(int CMC_flexion, int CMC_abduction, bool flip_abduction){
 	int servo1_offset = 0;
 
     //calculate how much the servos should offset from each other for abduction
-	if(CMC_abduction > 0){ //right
+
+	if(CMC_abduction < 0){ //right
 		CMC_abduction = max(CMC_abduction,THUMB_CMC_ABDUCTION_MIN);
 		servo1_offset=-CMC_abduction;
 	}else{ //left
 		CMC_abduction = min(CMC_abduction,THUMB_CMC_ABDUCTION_MAX);
 		servo0_offset=CMC_abduction;
 	}
+
+    //Serial.printf(">CMC_flexion:%d \n>CMC_abduction:%d \n>servo0_offset:%d \n>servo1_offset:%d \n",CMC_flexion, CMC_abduction, servo0_offset, servo1_offset);
 
     //option to flip abduction direction
     if(flip_abduction){
@@ -111,24 +115,14 @@ void controlFinger(uint8_t finger_num, uint8_t finger_pos[]){
     uint8_t M0_servo_num, M1_servo_num, T_servo_num;
     bool flip_abduction = false;
     switch(finger_num){
-        case 3: // Index Finger
-            PIP_flexion = finger_pos[11];
-            MCP_flexion = finger_pos[10];
-            MCP_abduction = finger_pos[9] + MCP_ABDUCTION_MIN;
-            flip_abduction = true; // Example logic
-            M0_servo_num = IM0_SERVO_PIN;
-            M1_servo_num = IM1_SERVO_PIN;
-            T_servo_num = IT_SERVO_PIN;
-            break;
-
-        case 2: // Middle Finger
-            PIP_flexion = finger_pos[8];
-            MCP_flexion = finger_pos[7];
-            MCP_abduction = finger_pos[6] + MCP_ABDUCTION_MIN;
-            flip_abduction = true;
-            M0_servo_num = MM0_SERVO_PIN;
-            M1_servo_num = MM1_SERVO_PIN;
-            T_servo_num = MT_SERVO_PIN;
+        case 0: // Pinkie Finger
+            PIP_flexion = finger_pos[2];
+            MCP_flexion = finger_pos[1];
+            MCP_abduction = finger_pos[0] + MCP_ABDUCTION_MIN;
+            flip_abduction = false;
+            M0_servo_num = PM0_SERVO_PIN;
+            M1_servo_num = PM1_SERVO_PIN;
+            T_servo_num = PT_SERVO_PIN;
             break;
 
         case 1: // Ring Finger
@@ -141,14 +135,24 @@ void controlFinger(uint8_t finger_num, uint8_t finger_pos[]){
             T_servo_num = RT_SERVO_PIN;
             break;
 
-        case 0: // Pinkie Finger
-            PIP_flexion = finger_pos[2];
-            MCP_flexion = finger_pos[1];
-            MCP_abduction = finger_pos[0] + MCP_ABDUCTION_MIN;
-            flip_abduction = false;
-            M0_servo_num = PM0_SERVO_PIN;
-            M1_servo_num = PM1_SERVO_PIN;
-            T_servo_num = PT_SERVO_PIN;
+        case 2: // Middle Finger
+            PIP_flexion = finger_pos[8];
+            MCP_flexion = finger_pos[7];
+            MCP_abduction = finger_pos[6] + MCP_ABDUCTION_MIN;
+            flip_abduction = true;
+            M0_servo_num = MM0_SERVO_PIN;
+            M1_servo_num = MM1_SERVO_PIN;
+            T_servo_num = MT_SERVO_PIN;
+            break;
+
+        case 3: // Index Finger
+            PIP_flexion = finger_pos[11];
+            MCP_flexion = finger_pos[10];
+            MCP_abduction = finger_pos[9] + MCP_ABDUCTION_MIN;
+            flip_abduction = true;
+            M0_servo_num = IM0_SERVO_PIN;
+            M1_servo_num = IM1_SERVO_PIN;
+            T_servo_num = IT_SERVO_PIN;
             break;
 
         default: // Unknown Finger
@@ -209,6 +213,7 @@ void moveServoThroughSerial(){
 
 void printEmaxServoPositions(){
     servo_position_num=0;
+
     Serial.printf(">PM0:%d \n>PM1:%d \n>PT:%d \n",all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++]);
     Serial.printf(">RM0:%d \n>RM1:%d \n>RT:%d \n",all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++]);
     Serial.printf(">MM0:%d \n>MM1:%d \n>MT:%d \n",all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++], all_servo_positions[servo_position_num++]);
--- code/RoboticArm/test/ArmControlLibTest/lib/RoboticArm/RoboticArm.h
+++ code/RoboticArm/test/ArmControlLibTest/lib/RoboticArm/RoboticArm.h
@@ -7,7 +7,7 @@
 
 #define PEER_MAC {0x54, 0x32, 0x04, 0x89, 0x70, 0x54}
 #define ENABLE_PRESSURE_PRINT 0
-#define ENABLE_CONTROL_PRINT 1 
+#define ENABLE_CONTROL_PRINT 1
 
 void pressureSensorCode(void* params);
 void armControlCode(void* params);
