b523ec38cf27cfe654d7c5438d0c715413e18474
wmk7fe <wmk7fe@virginia.edu>
2024-12-08 16:30:27 -0500

    added ISR tracking to Robotic Arm

--- code/RoboticArm/lib/PressureSensors/PressureSensors.cpp
+++ code/RoboticArm/lib/PressureSensors/PressureSensors.cpp
@@ -6,9 +6,11 @@ uint8_t fm;
 uint8_t fr;
 uint8_t fp;
 uint8_t ft;
+uint8_t k;
+unsigned long time1_elapsed;
 
 void setupPressureSensors(){
-    return;
+    k = 0;
 }
 
 // IMPORTANT: IMPLEMENT PRESSURE SENSOR READINGS
@@ -28,5 +30,11 @@ void sendPressureData(){
     }
     pollPressureSensors();
     arm_sendData(fi, fm, fr, fp, ft);
-    return;
+    
+    k++;
+    if(k%200 == 0 && TRACK_ISR_1){
+    Serial.print("\ncallback 1 run at "); Serial.print((200*1000000)/(micros() - time1_elapsed)); Serial.println(" Hz\n");
+    k = 0;
+    time1_elapsed = micros();
+  }
 }
