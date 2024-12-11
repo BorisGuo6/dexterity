b9e914ff362e49eeb8c95acf01dc7dce09b65677
wmk7fe <wmk7fe@virginia.edu>
2024-12-11 14:28:07 -0500

    changes

--- code/HapticGlove/lib/HapticGlove/HapticGlove.h
+++ code/HapticGlove/lib/HapticGlove/HapticGlove.h
@@ -18,8 +18,8 @@
 #define ENABLE_SENSORS_PRINT 0
 #define ENABLE_IMU_SENSORS_PRINT 0
 #define ENABLE_ESPNOW_PRINT 1
-#define TRACK_ISR_0 0
-#define TRACK_ISR_1 0
+#define TRACK_ISR_0 1
+#define TRACK_ISR_1 1
 #define ISR0_FREQ 50 // in Hz
 #define ISR1_FREQ 50 // in Hz
 
